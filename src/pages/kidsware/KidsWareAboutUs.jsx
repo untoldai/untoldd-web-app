13959571a9d8384f83d633ce2757da25993cc886
Gaurav01Kumar <gk1427473@gmail.com>
2024-12-16 08:40:15 +0530

    short

--- src/pages/kidsware/KidsWareAboutUs.jsx
+++ src/pages/kidsware/KidsWareAboutUs.jsx
@@ -18,8 +18,8 @@ const BeautyAboutUs = () => {
         </div>
         <div className='max-w-xl'>
           <h3 className='text-3xl font-bold '></h3>
-          <p className="text-slate-600 text-lg my-4 max-w-3xl font-medium tracking-wide leading-relaxed">Untoldd to be toldd
-             At Untoldd, our mission is to empower creators by transforming their unique visions into thriving, sustainable brands. We believe that every creator has an untold story, a brand waiting to be born. Our goal is to make brand creation accessible, seamless, and impactful by providing end-to-end support—from manufacturing and packaging to marketing and distribution.
+          <p className="text-slate-600 text-lg my-4 max-w-3xl font-medium tracking-wide leading-relaxed">
+             Our mission is to empower creators by transforming their unique visions into thriving, sustainable brands. We believe that every creator has an untold story, a brand waiting to be born. Our goal is to make brand creation accessible, seamless, and impactful by providing end-to-end support—from manufacturing and packaging to marketing and distribution.
             We are committed to redefining the traditional model of brand building by partnering with influencers, giving them the tools and resources they need to succeed. We aim to democratize entrepreneurship, allowing creators to turn their passions into profitable businesses, and bring innovative, high-quality products to consumers worldwide.
             Untoldd is more than just a business; it’s a platform for creators to share their stories, build legacies, and inspire others. Our mission is to make these untold stories a reality, one brand at a time.</p>
         </div>
